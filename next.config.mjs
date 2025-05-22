dade4eebaf8cf0b3102cd94dff4a8c679ce7dde4
edd1esson <1214523618@qq.com>
2025-05-22 01:22:07 -0400

    initial project commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  experimental: {
+    //appDir: true,
+    swcPlugins: [
+      ["next-superjson-plugin", {}]
+    ]
+  },
+  images: {
+    domains:[
+      "res.cloudinary.com",
+      "avatars.githubusercontent.com",
+      "klh3.googleusercontent.com"
+    ]
+  }
+}
+  
 export default nextConfig;
+  
